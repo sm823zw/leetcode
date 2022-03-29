f2797d3cc73c852e6f7208da28a332298261f9ae
Saankhya Mondal <49569284+sm823zw@users.noreply.github.com>
2022-03-29 23:27:22 +0530

    Time: 16 ms (61.04%), Space: 9.6 MB (31.30%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    
+    int dx[4] = {-1, 0, 0, 1};
+    int dy[4] = {0, -1, 1, 0};
+    
+    
+    void dfs(vector<vector<int>>& grid, int r, int c, int count){
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if(r<0 || r>=m || c<0 || c>=n){
+            return;
+        }
+        if(grid[r][c] < 0 || grid[r][c] == 1){
+            return;
+        }
+        grid[r][c] = -count;
+        for(int i=0;i<4;i++){
+            dfs(grid, r+dx[i], c+dy[i], count);
+        }
+    }
+    
+    
+    int closedIsland(vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 0){
+                    dfs(grid, i, j, count);
+                    count++;
+                }
+            }
+        }
+        if(count == 0){
+            return 0;
+        }
+        count--;
+        unordered_set<int>st;
+        for(int i=0;i<m;i++){
+            if(grid[i][0]<0){
+                st.insert(grid[i][0]);
+            }
+            if(grid[i][n-1]<0){
+                st.insert(grid[i][n-1]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[0][i]<0){
+                st.insert(grid[0][i]);
+            }
+            if(grid[m-1][i]<0){
+                st.insert(grid[m-1][i]);
+            }
+        }
+        return count - st.size();
+    }
+};
